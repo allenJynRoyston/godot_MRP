29cee26f8179678b8b2e674089efe03bd0c05066
Jyn <jyn@Jyns-MacBook-Air.local>
2024-12-17 22:43:19 +0100

    updated

--- Scenes/TrainingProgram/parts/GameplayLoop/parts/StoreContainer/store_container.gd
+++ Scenes/TrainingProgram/parts/GameplayLoop/parts/StoreContainer/store_container.gd
@@ -1,13 +1,40 @@
 @tool
 extends GameContainer
 
-@onready var CloseBtn:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer2/CloseBtn
+@onready var CloseBtn:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer/CloseBtn
 @onready var GridItemContainer:GridContainer = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/PanelContainer/MarginContainer/ScrollContainer/MarginContainer/GridContainer
+@onready var FocusDetails:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/FocusDetails
+@onready var CategoryContainer:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer/CategoryContainer
 
-#@onready var NextBtn:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer/NextBtn
-#@onready var PreviousBtn:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer/PreviousBtn
-#@onready var SubmitBtn:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer2/SubmitBtn
-#@onready var BackBtn:Control = $Back
+@onready var PaginationBack:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer2/PaginationBackBtn
+@onready var PaginationMore:Control = $SubViewport/PanelContainer/MarginContainer/PanelContainer/HBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/HBoxContainer2/PaginationMoreBtn
+
+const StoreItemPreload:PackedScene = preload("res://Scenes/TrainingProgram/parts/GameplayLoop/parts/StoreContainer/parts/StoreItem/StoreItem.tscn")
+const TextBtnPreload:PackedScene = preload("res://UI/Buttons/TextBtn/TextBtn.tscn")
+
+var resources_data:Dictionary = {}
+
+var focus_data:Dictionary = {} : 
+	set(val):
+		focus_data = val
+		on_focus_data_update()
+
+var filters:Dictionary = {} : 
+	set(val):
+		filters = val
+		on_filters_update()
+
+var active_filters:Array = [] : 
+	set(val):
+		active_filters = val
+		
+var is_busy:bool = false
+var pagination:int = 0 : 
+	set(val):
+		pagination = val
+		on_pagination_update()
+		
+var limit:int = 6
 
 signal user_response
 
@@ -17,20 +44,113 @@ func _ready() -> void:
 	
 	TextureRectNode = $TextureRect
 	Subviewport = $SubViewport
-	
+
 	CloseBtn.onClick = func() -> void:
 		user_response.emit({"action": ACTION.BACK})
 		
+	PaginationBack.onClick = func() -> void:
+		pagination = pagination - 1
+	PaginationMore.onClick = func() -> void:
+		pagination = pagination + 1
+		
+
+	var category_data:Dictionary = ROOM_UTIL.get_categories()
+	for item in category_data.list:
+		var new_node:Control = TextBtnPreload.instantiate()
+		new_node.title = item.details.name
+		new_node.icon = SVGS.TYPE.MINUS
+		new_node.onClick = func() -> void:
+			if !is_busy:
+				filters[item.id].active = !filters[item.id].active
+				filters = filters
+				build_list()
+			
+		CategoryContainer.add_child(new_node)
+		
+		filters[item.id] = {
+			"active": false, 
+			"node_ref": new_node
+		}
+	filters = filters
+
+	on_focus_data_update()
+	on_filters_update()
 	
-	for index in GridItemContainer.get_child_count():
-		var node:Control = GridItemContainer.get_child(index)
-		node.onClick = func() -> void:
+# --------------------------------------------------------------------------------------------------		
+
+# --------------------------------------------------------------------------------------------------		
+func start() -> void:	
+	build_list()
+# --------------------------------------------------------------------------------------------------			
+
+# --------------------------------------------------------------------------------------------------			
+func on_pagination_update() -> void:
+	build_list()
+# --------------------------------------------------------------------------------------------------				
+
+# --------------------------------------------------------------------------------------------------			
+func build_list() -> void:
+	var start_at:int = pagination * limit
+	var list_data:Dictionary = ROOM_UTIL.get_rooms(active_filters, start_at, limit)
+
+	PaginationBack.hide() if pagination == 0 else PaginationBack.show()
+	PaginationMore.show() if list_data.has_more else PaginationMore.hide()
+	
+	for child in GridItemContainer.get_children():
+		child.queue_free()
+
+	for item in list_data.list:
+		var new_node:Control = StoreItemPreload.instantiate()
+		new_node.resources_data = resources_data
+		new_node.data = item
+		
+		new_node.onClick = func() -> void:
 			user_response.emit({
 				"action": ACTION.NEXT, 
 				"selected": {
 					"uid": U.generate_uid(),
-					"room_id": ROOM.TYPE.BARRICKS
+					"id": item.id
 				}
 			})
+		
+		new_node.onFocus = func(node:Control) -> void:			
+			update_focus_data.call_deferred(item)
+		
+		new_node.onBlur = func(node:Control) -> void:
+			update_focus_data.call({})
+			
+		GridItemContainer.add_child(new_node)	
+		
+	await U.set_timeout(0.2)
+	is_busy = false		
+# --------------------------------------------------------------------------------------------------			
+
+# --------------------------------------------------------------------------------------------------			
+func end() -> void:
+	for child in GridItemContainer.get_children():
+		child.queue_free()
+# --------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------
+func on_filters_update() -> void:
+	var _active_filters = []
+	for key in filters:
+		var item:Dictionary = filters[key]
+		item.node_ref.icon = SVGS.TYPE.PLUS if item.active else SVGS.TYPE.MINUS
+		if item.active:
+			_active_filters.push_back(key)
+			
+	active_filters = _active_filters
+# --------------------------------------------------------------------------------------------------				
 
 # --------------------------------------------------------------------------------------------------		
+func update_focus_data(new_state:Dictionary):
+	focus_data = new_state
+# --------------------------------------------------------------------------------------------------		
+		
+# --------------------------------------------------------------------------------------------------		
+func on_focus_data_update() -> void:
+	if is_node_ready():
+		FocusDetails.hide_details = !focus_data.is_empty()
+		FocusDetails.data = focus_data
+# --------------------------------------------------------------------------------------------------			
