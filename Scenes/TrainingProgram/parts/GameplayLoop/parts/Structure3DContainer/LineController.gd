67bc97dc36c5bf93438560361a2b801ecc809350
Jyn Royston <allen.acr@gmail.com>
2024-12-21 18:14:44 +0100

    added lines

--- Scenes/TrainingProgram/parts/GameplayLoop/parts/Structure3DContainer/LineController.gd
+++ Scenes/TrainingProgram/parts/GameplayLoop/parts/Structure3DContainer/LineController.gd
@@ -1,17 +1,17 @@
 extends Control
 
-var start_point: Vector2 : 
-	set(val):
-		start_point = val
-		redraw()
+var start_point: Vector2 
+var end_point: Vector2 
 		
-var end_point: Vector2 : 
-	set(val):
-		end_point = val
-		redraw()
-		
-var line_color: Color = Color(1, 0, 0)  # Red color
-var line_width: float = 2.0
+@export var line_color: Color = Color(1, 0, 0)  # Red color
+@export var line_width: float = 2.0
+
+func _draw():	
+	draw_line(start_point, end_point, line_color, line_width)
+
+func update_line_vectors(sp:Vector2, ep:Vector2) -> void:
+	start_point = sp
+	end_point = ep
 
 func redraw():	
 	if !is_node_ready():return
--- Scenes/TrainingProgram/parts/GameplayLoop/parts/Structure3DContainer/structure_3d_container.gd
+++ Scenes/TrainingProgram/parts/GameplayLoop/parts/Structure3DContainer/structure_3d_container.gd
@@ -1,13 +1,17 @@
 @tool
 extends GameContainer
 
-@onready var ZoomA:BtnBase = $TextureRect2/PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomA
-@onready var ZoomB:BtnBase = $TextureRect2/PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomB
-@onready var ZoomC:BtnBase = $TextureRect2/PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomC
+@onready var ZoomA:BtnBase = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomA
+@onready var ZoomB:BtnBase = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomB
+@onready var ZoomC:BtnBase = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomC
+@onready var ZoomD:BtnBase = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/MarginContainer/VBoxContainer/HBoxContainer/ZoomD
 
-@onready var FloatingPointContainer:Control = $TextureRect2/PanelContainer/MarginContainer/PanelContainer/FloatingPointContainer
-@onready var TestPoint:Control = $TextureRect2/PanelContainer/MarginContainer/PanelContainer/FloatingPointContainer/TestPoint
-@onready var LineDrawController:Control = $TextureRect2/PanelContainer/MarginContainer/PanelContainer/LineDrawController
+@onready var OverlayContainer:Control = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer
+@onready var FloatingInfo:Control = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/FloatingInfo
+@onready var TestPoint:Control = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/FloatingInfo/TestPoint
+@onready var LineDrawController:Control = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/LineDrawController
+
+@onready var RoomName:Label = $TextureRect2/PanelContainer/MarginContainer/OverlayContainer/FloatingInfo/TestPoint/VBoxContainer/MarginContainer/RoomName
 
 @onready var RenderLayer1:Node3D = $SubViewport/Rendering
 @onready var RenderLayer2:Node3D = $SubViewport2/Rendering
@@ -33,28 +37,22 @@ func _exit_tree() -> void:
 	GBL.unregister_node(REFS.STRUCTURE_3D)
 	GBL.unsubscribe_to_process(self)
 	
-func _draw():
-	# Draw a line between the start and end points
-	var start_point: Vector2 = Vector2(50, 50)
-	var end_point: Vector2 = Vector2(150, 150)
-	var line_color: Color = Color(1, 0, 0)  # Red color
-	var line_width: float = 2.0	
-	draw_line(start_point, end_point, line_color, line_width)	
-	
 func _ready() -> void:
 	super._ready()
 	TextureRectNode = $TextureRect
 	Subviewport = $SubViewport
 	
 	ZoomA.onClick = func() -> void:
-		pass
-		#camera_type = CAMERA.TYPE.PERSPECTIVE
+		GBL.find_node(REFS.GAMEPLAY_LOOP).current_camera_zoom = CAMERA.ZOOM.OVERVIEW
+		
 	ZoomB.onClick = func() -> void:
-		pass
-		#$camera_type = CAMERA.TYPE.OVERHEAD
+		GBL.find_node(REFS.GAMEPLAY_LOOP).current_camera_zoom = CAMERA.ZOOM.FLOOR
+		
 	ZoomC.onClick = func() -> void:
-		pass
-		#camera_type = CAMERA.TYPE.OVERHEAD		
+		GBL.find_node(REFS.GAMEPLAY_LOOP).current_camera_zoom = CAMERA.ZOOM.RING
+		
+	ZoomD.onClick = func() -> void:
+		GBL.find_node(REFS.GAMEPLAY_LOOP).current_camera_zoom = CAMERA.ZOOM.RM
 	
 	on_camera_type_update()
 	on_current_camera_zoom_update()
@@ -62,14 +60,43 @@ func _ready() -> void:
 
 # --------------------------------------------------------------------------------------------------		
 func on_current_camera_zoom_update() -> void:
+	for node in [ZoomA, ZoomB, ZoomC, ZoomD]:
+		node.inactive_color = Color(0.292, 0.292, 0.292, 0.827)
+	
+	match current_camera_zoom:
+		CAMERA.ZOOM.OVERVIEW:
+			ZoomA.inactive_color = Color.WHITE
+		CAMERA.ZOOM.FLOOR:
+			ZoomB.inactive_color = Color.WHITE
+		CAMERA.ZOOM.RING:
+			ZoomC.inactive_color = Color.WHITE
+		CAMERA.ZOOM.RM:
+			ZoomD.inactive_color = Color.WHITE			
+			
+
 	for node in [RenderLayer1, RenderLayer2]:
 		node.current_camera_zoom = current_camera_zoom
+	
+	
+		
 # --------------------------------------------------------------------------------------------------			
 	
 # --------------------------------------------------------------------------------------------------
 func on_current_location_update() -> void:
+	if !is_node_ready():return
 	for node in [RenderLayer1, RenderLayer2]:
 		node.current_location = current_location
+	
+	if room_config.is_empty():return
+	var floor:int = current_location.floor
+	var ring:int = current_location.ring
+	var room:int = current_location.room		
+	var room_data:Dictionary = room_config.floor[floor].ring[ring].room[room].room_data
+	if room_data.is_empty():
+		RoomName.text = "Empty"
+	else:
+		RoomName.text = room_data.get_room_data.call().name
+	#RoomName.text = 
 # --------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------------------------------
@@ -94,8 +121,7 @@ func on_control_input_update(input_data:Dictionary) -> void:
 # --------------------------------------------------------------------------------------------------
 func on_process_update(delta:float) -> void:
 	if !is_node_ready():return		
-	var active_room_pos:Vector2 = U.convert_from_normalized_position(FloatingPointContainer.size, GBL.get_projected_3d_object_normalized_position("active_room"))
-	TestPoint.position = active_room_pos - Vector2(0, 20)
-	
-	LineDrawController.start_point = active_room_pos
+	var active_room_pos:Vector2 = U.convert_from_normalized_position(FloatingInfo.size, GBL.get_projected_3d_object_normalized_position("active_room"))
+	TestPoint.position = active_room_pos - Vector2(TestPoint.size.x + 50, 40)
+	LineDrawController.update_line_vectors(active_room_pos, TestPoint.global_position - LineDrawController.global_position + TestPoint.size/2 )	
 # --------------------------------------------------------------------------------------------------
