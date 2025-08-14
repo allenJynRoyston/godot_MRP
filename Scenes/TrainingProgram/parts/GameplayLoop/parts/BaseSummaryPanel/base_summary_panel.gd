f1ab16093935d6265353dd9c14a694efe389b5d8
Jyn <allen.acr@gmail.com>
2025-08-14 22:50:12 +0200

    update action bar

--- Scenes/TrainingProgram/parts/GameplayLoop/parts/BaseSummaryPanel/base_summary_panel.gd
+++ Scenes/TrainingProgram/parts/GameplayLoop/parts/BaseSummaryPanel/base_summary_panel.gd
@@ -1,1 +1,15 @@
+@tool
 extends SubscribeWrapper
+
+@onready var List:HBoxContainer = $List
+
+func _ready() -> void:
+	for node in List.get_children():
+		node.hide()
+
+func on_current_location_update(new_val:Dictionary) -> void:
+	if !is_node_ready() or new_val.is_empty(): return
+	for index in List.get_child_count():
+		var panel_node:Control = List.get_child(index)
+		panel_node.show()
+		panel_node.show_content = new_val.ring == index
